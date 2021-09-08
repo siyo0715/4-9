09ba708dd4f3c767ec6ae3ce8f9fc8d01d9a00ed
EC2 Default User <ec2-user@ip-172-31-12-102.ec2.internal>
2021-09-08 14:51:52 +0000

    [Finish4-9]

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,12 @@
+class HomesController < ApplicationController
+  def index
+    @books = Book.all
+  end
+  def show
+    @book = Book.find(params[:id])
+  end
+  def new
+    @book = Book.new
+  end
+
+end
